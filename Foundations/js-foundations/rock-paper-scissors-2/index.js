eb1932bfd8d3260737d1438783644f2893681986
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-25 16:58:53 +0200

    Complete rock-paper-scissors project

--- Foundations/js-foundations/rock-paper-scissors-2/index.js
+++ Foundations/js-foundations/rock-paper-scissors-2/index.js
@@ -1,21 +1,65 @@
-function getComputerChoice (computerChoiceInt, computerChoiceHand = "")  {
+function getComputerChoice (computerChoiceInt, computerChoice = "")  {
   computerChoiceInt = Math.random();  
-  console.log(computerChoiceInt);
 
-  computerChoiceInt <= 1/3 ? computerChoiceHand = "Rock" : 
-  computerChoiceInt > 1/3 && computerChoiceInt <= 2/3 ? computerChoiceHand = "Paper" :
-  computerChoiceHand = "Scissors";
-  return computerChoiceHand;
+  computerChoiceInt <= 1/3 ? computerChoice = "Rock" : 
+  computerChoiceInt > 1/3 && computerChoiceInt <= 2/3 ? computerChoice = "Paper" :
+  computerChoice = "Scissors";
+  computerChoice = computerChoice.toLowerCase();
+  return computerChoice;
 }
 
-function getHumanChoice (humanChoiceHand) {
-  humanChoiceHand = prompt("Please enter a choice: Rock, Paper or Scissors");
-  humanChoiceHand = humanChoiceHand.toLowerCase();
-  while (humanChoiceHand != "rock" && humanChoiceHand != "paper" && humanChoiceHand != "scissors") {
-    humanChoiceHand = prompt("Please enter a valid choice");
-    humanChoiceHand = humanChoiceHand.toLowerCase();
+function getHumanChoice (humanChoice) {
+  humanChoice = prompt("Please enter a choice: Rock, Paper or Scissors");
+  humanChoice = humanChoice.toLowerCase();
+  while (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
+    humanChoice = prompt("Please enter a valid choice");
+    humanChoice = humanChoice.toLowerCase();
   }
-  return humanChoiceHand;
+  return humanChoice;
 }
 
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log(humanChoice, computerChoice);
+    console.log("It's a tie");
+  } else if (humanChoice == "rock" && computerChoice == "paper") {
+    console.log("You loose");
+    computerScore++;
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log("You win");
+    humanScore++;
+  } else if (humanChoice == "paper" && computerChoice == "scissors") {
+    console.log("You loose");
+    computerScore++;
+  }else if (humanChoice == "paper" && computerChoice == "rock") {
+    console.log("You win");
+    humanScore++;
+  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+    console.log("You loose");
+    computerScore++;
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log("You win");
+    humanScore++;
+  }
+}
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    
+    playRound(humanChoice, computerChoice);
+    console.log(`Human chose ${humanChoice}, Computer chose ${computerChoice}`)
+    console.log(`Human score ${humanScore}`)
+    console.log(`Computer score ${computerScore}`)
+  }
+}
+
+playGame();
