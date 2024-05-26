370a5f189ad300332be5fbb170eca0b43f123ae1
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-26 13:38:19 +0200

    Add alert for each button click

--- Foundations/js-foundations/DOM-exercise.js
+++ Foundations/js-foundations/DOM-exercise.js
@@ -31,3 +31,15 @@ const btn2 = document.querySelector("#btn2");
 btn2.addEventListener("click", () => {
   alert("Hello World");
 })
+
+btn2.addEventListener("click", function(e){
+  e.target.style.background = "blue";
+})
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    alert(button.id);
+  })
+})
